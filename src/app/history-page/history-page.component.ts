8e09d114415523fd8f1e5530f9e5a452443f470f
chris <ch.fries@gmx.net>
2025-01-18 15:09:52 +0100

    feat(29): Implement story-element

--- src/app/history-page/history-page.component.ts
+++ src/app/history-page/history-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {HeaderElementComponent} from './header-element/header-element.component';
+import {StoryElementComponent} from './story-element/story-element.component';
 
 @Component({
   selector: 'app-history-page',
   imports: [
-    HeaderElementComponent
+    HeaderElementComponent,
+    StoryElementComponent
   ],
   templateUrl: './history-page.component.html',
   styleUrl: './history-page.component.css'
