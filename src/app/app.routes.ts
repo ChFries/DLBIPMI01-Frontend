4e38ce4304f74494bbcd1700ae50324662c093d2
chris <ch.fries@gmx.net>
2025-01-21 19:29:02 +0100

    feat(42): Add disclaimer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import {HistoryPageComponent} from './history-page/history-page.component';
+import {WarningPageComponent} from './warning-page/warning-page.component';
 
 export const routes: Routes = [
   {path: 'history', title: 'Firmenhistorie', component: HistoryPageComponent},
-  {path: '**', redirectTo: ""}
+  {path: '**', component: WarningPageComponent}
 ];
